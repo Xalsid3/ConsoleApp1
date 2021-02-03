8deec89a9bd199179f86de8e9785d51e978f538c
Rin Kaoki <lemi013794@gmail.com>
2021-02-03 11:36:48 +0700

    0.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("cAlculat");
+            Console.WriteLine("Введите первое слово");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Введите второе слово");
+            string s2 = Console.ReadLine();
+            string sum = s1 + " " + s2;
+            Console.WriteLine(sum);
             Console.ReadKey();
         }
     }
